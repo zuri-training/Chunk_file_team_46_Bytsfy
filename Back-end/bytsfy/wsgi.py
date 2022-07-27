486e1c72d1b0b61d37294fafa596c5397073eca2
User <foodhubnig@gmail.com>
2022-07-27 22:27:06 +0100

    m

--- Back-end/bytsfy/wsgi.py
+++ Back-end/bytsfy/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for bytesfile project.
+WSGI config for bytsfy project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bytesfile.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bytsfy.settings')
 
 application = get_wsgi_application()
