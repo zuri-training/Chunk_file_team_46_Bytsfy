2961156ec9387a2c0ec947e663fb9910f96fd499
User <foodhubnig@gmail.com>
2022-08-07 10:12:34 +0100

    m

--- Back-end/bytsfy/settings.py
+++ Back-end/bytsfy/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY1')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
