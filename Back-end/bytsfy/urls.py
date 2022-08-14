5a8b8d4cc82a6aa76687b8f0f12235abd627f712
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 <yusufabdulsamad46@gmail.com>
2022-08-14 15:16:52 +0100

    commit

--- Back-end/bytsfy/urls.py
+++ Back-end/bytsfy/urls.py
@@ -36,8 +36,8 @@ urlpatterns = [
     # path("contact", TemplateView.as_view(template_name="contact.html"), name="contact"),     #included in chunked_files app
     # path("upload", TemplateView.as_view(template_name="upload.html"), name="upload"),
     # path("getStarted", TemplateView.as_view(template_name="getStarted.html"), name="about"),
-    path("chunk/", include("chunked_files.urls")),
-    path("", include('chunker.urls')),
+    # path("chunk/", include("chunked_files.urls")),
+    # path("", include('chunker.urls')),
     # path("",include('chunked_files.urls')),
 
 
